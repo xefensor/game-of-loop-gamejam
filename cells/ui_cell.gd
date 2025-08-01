fcd9f68007e539288b1c6bd08e6f2cf15b39f4a5
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-08-01 22:05:04 +0200

    Add mouse drag accros grid

--- cells/ui_cell.gd
+++ cells/ui_cell.gd
@@ -2,18 +2,28 @@ class_name UICell
 extends ColorRect
 
 var grid_position := Vector2i.ZERO
+var mouse_on_top: bool = false
 
 signal cell_selected(UICell)
 
 func _init(_grid_position: Vector2i) -> void:
 	grid_position = _grid_position
-	gui_input.connect(_on_gui_input)
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 	
 	size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	size_flags_vertical = Control.SIZE_EXPAND_FILL
 	mouse_filter = Control.MOUSE_FILTER_PASS
-	
-	
-func _on_gui_input(event: InputEvent):
-	if event.is_pressed():
+
+
+func _process(delta: float) -> void:
+	if mouse_on_top and Input.is_action_pressed("click"):
 		cell_selected.emit(self)
+
+
+func _on_mouse_entered():
+	mouse_on_top = true
+	
+	
+func _on_mouse_exited():
+	mouse_on_top = false
